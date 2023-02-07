be24fd37dacce74b94b79b18d23ee27f10972aa7
Stefan Dimitrov <stefan.r7.dimitrov@gmail.com>
2023-02-07 22:12:31 +0200

    adding clips posting functionality

--- src/lib/domain/clip/field/shortcode.rs
+++ src/lib/domain/clip/field/shortcode.rs
@@ -1,43 +1,12 @@
 use crate::domain::clip::ClipError;
 use derive_more::From;
+use rocket::{UriDisplayPath, UriDisplayQuery};
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
-#[derive(Debug, Clone, Deserialize, Serialize, From)]
+#[derive(Debug, Clone, Deserialize, Serialize, From, UriDisplayPath, UriDisplayQuery)]
 pub struct ShortCode(String);
 
-
-// That was the errored version on the tutorial
-// impl ShortCode {
-//     pub fn new() -> Self {
-//         use rand::prelude::*;
-//         let allowed_chars = [
-//             'a', 'b', 'c', 'd', '1', '2', '3', '4'
-//         ];
-
-//         let mut rng = thread_rng();
-//         let mut shortcode = String::with_capacity(10);
-//         for _ in 0..10 {
-//             shortcode.push(
-//                 *allowed_chars
-//                     .choose(&mut rng)
-//                     .expect("sampling array should have values"),
-//             );
-//             Self(shortcode);
-//         }
-
-//         pub fn as_str(&self) -> &str {
-//             self.0.as_str()
-//         }
-
-//         pub fn into_inner(self) -> String{
-//             self.0
-//         }
-//     }
-// }
-
-
-// that is my fix - functionality needs to be tested
 impl ShortCode {
     pub fn new() -> Self {
         use rand::prelude::*;
@@ -102,3 +71,4 @@ impl FromStr for ShortCode {
         Ok(Self(s.into()))
     }
 }
+
